dff0ca4a322a40f34fb35b9bc00a5d8d77eef4ac
charles.song <charles.song@yijinin.com>
2024-06-21 15:27:09 +0800

    修改构建脚本

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 export XK6_K6_REPO=github.com/shlsky/k6
-xk6  build v0.45.4 --with  github.com/shlsky/xk6-grpc@v0.0.13 --with github.com/avitalique/xk6-file@latest  --with github.com/mostafa/xk6-kafka@latest --with github.com/grafana/xk6-redis --with github.com/shlsky/xk6-nacos=.
+#xk6  build v0.45.4 --with  github.com/shlsky/xk6-grpc@v0.0.13 --with github.com/avitalique/xk6-file@latest  --with github.com/mostafa/xk6-kafka@latest --with github.com/grafana/xk6-redis --with github.com/shlsky/xk6-nacos=.
+
+xk6  build v0.45.4 --with  github.com/shlsky/xk6-grpc@v0.0.13 --with github.com/avitalique/xk6-file@latest  --with github.com/mostafa/xk6-kafka@latest --with github.com/grafana/xk6-redis --with github.com/shlsky/xk6-nacos@v0.0.3
